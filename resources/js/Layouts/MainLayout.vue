6ba6c5be9645e2a5f6ddec5efd930fa56b46f665
Golvan98 <gilvinsz@gmail.com>
2023-07-04 03:37:56 -0400

    added raw content for the rightside of the footer

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
  <header class="dark:border-gray-700 bg-green-500 dark:bg-gray-800 w-full h-24">
     
@@ -35,7 +35,7 @@
       <div class="w-full h-4/5 flex"> 
 
         <div class="w-1/2 h-full flex items-center justify-between">
-          <div class="w-3/4 h-full flex justify-between">
+          <div class="w-3/4 h-full flex justify-between ml-12">
 
            <div class="w-1/3 mt-6 ml-12 text-white">
              Community <br> Discord <br> Reddit <br> Github <br> Guide  
@@ -46,13 +46,18 @@
            </div>
 
            <div class="w-1/3 mt-6 ml-12 text-white">
-            About Us <br> Open Source <br> Github Profile <br> About Us <br>  
+            About Us <br> Open Source <br> Github Profile <br> Contact Us <br>  
            </div>
 
           </div>
         </div>
 
-        <div class="w-1/2 h-full"> 1 </div>
+        <div class="w-1/2 h-full text-white text-1xl "> 
+          <div class="mt-5 border-t"> GBK is a booking app created by Gilvin Zalsos as his magnum opus in his endeavor of mastering web development using laravel, inertia, * vuejs. </div>
+          <div class="mt-5"> This project will likely stay on going as I intend to test numerous features. Features can be random but relevant to the purpose of this project (which is to function as a 1v1 consulting booking app for all kinds of professionals) </div>
+          <div class="mt-2"> This project will likely stay on going as I intend to test numerous features. Features can be random but relevant to the purpose of this project (which is to function as a 1v1 consulting booking app for all kinds of professionals) </div>
+
+        </div>
       </div>
 
     </div>  
