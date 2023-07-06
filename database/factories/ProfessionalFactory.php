c26339df8a5a7c692ddd73835e4f8fdcebee2c13
Golvan98 <gilvinsz@gmail.com>
2023-07-06 08:25:04 -0400

    first draft for Professional Table DB Seeder

---
+++ database/factories/ProfessionalFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Professional>
+ */
+class ProfessionalFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'firstName' => fake()->firstName(),
+            'lastName' => fake()->lastName(),
+            'profession' =>fake()->randomElement(['Lawyer', 'Doctor', 'Engineer', 'Software Engineer', 'Accountant', 'English Tutor', 'Professor']),
+            'bio' => fake()->sentence(),
+        ];
+    }
+}
