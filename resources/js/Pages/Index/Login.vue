8dbf14261c9ed0297979dd22d92cf9ea17990eb6
Golvan98 <gilvinsz@gmail.com>
2023-07-10 05:56:14 -0400

    added a centered div template for login page

--- resources/js/Pages/Index/Login.vue
+++ resources/js/Pages/Index/Login.vue
@@ -1,11 +1,28 @@
 <template>
 
-<body class="h-screen w-full bg-grey-300">
+<body class="h-screen w-full bg-grey-300 flex-nowrap">
 
-<div class="w-full h-full flex-nowrap justify-center">
-    <div class="w-full h-1/2 bg-red-300"> 1</div>
-    <div class="w-full h-1/2 bg-red-400"> 1</div>
-</div>    
+ <div id="main" class="w-full h-full bg-grey-300 flex items-center justify-center"> 
+
+  <div class="bg-grey-500 w-1/4 h-3/5 border border-red-300 mb-12 flex-nowrap"> 
+
+    <div class="bg-red-300 h-1/5">
+            hello
+    </div>
+
+    <div class="bg-red-300 h-3/5">
+            hello 2
+    </div>
+
+    <div class="bg-red-300 h-1/5">
+            hello 3
+    </div>
+
+  </div>
+
+
+ </div>
+    
 </body>
 
 
