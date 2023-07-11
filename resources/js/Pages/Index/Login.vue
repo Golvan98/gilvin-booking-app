f80f58851c1bd77bf24e59fb738aa9d96d618d2f
Golvan98 <gilvinsz@gmail.com>
2023-07-11 08:09:40 -0400

    login form halfway through

--- resources/js/Pages/Index/Login.vue
+++ resources/js/Pages/Index/Login.vue
@@ -2,16 +2,41 @@
 
 <body class="h-screen w-full bg-grey-300 flex-nowrap">
 
- <div id="main" class="w-full h-full bg-grey-300 flex items-center justify-center"> 
+ <div id="main" class="w-full h-full bg-gray-100 flex items-center justify-center"> 
 
-  <div class="bg-grey-500 w-1/4 h-3/5 border border-red-300 mb-12 flex-nowrap"> 
+  <div class="w-1/4 h-3/5 border mb-12 flex-nowrap shadow-lg"> 
 
-    <div class="bg-red-300 h-1/5">
-            hello
+    <div class="h-1/5 bg-white flex items-center justify-center text-2xl text-gray-400">
+        <div class="h-1/2">𝔾𝔹𝕂 </div>
     </div>
 
-    <div class="bg-red-300 h-3/5">
-            hello 2
+    <div class=" bg-white h-3/5">
+           
+
+
+            <form class="h-full flex-nowrap">
+
+                <div class="flex justify-start border-t border-red-300">
+                        <label class="my-2  ml-4 font-bold" for="email"> Email </label>
+                </div>
+
+                <div class="w-full flex items-center justify-center mt-0.5"> 
+                        <input type="email" name="email" id="email" class="ml-4 mr-4 w-full rounded-sm" placeholder="name@company.com" required="">
+                </div>
+
+                <div class="flex justify-start mt-6">
+                        <label class="my-2 ml-4 font-bold" for="password"> Password </label>
+                </div>
+
+                <div class="w-full flex items-center justify-center mt-0.5"> 
+                        <input type="password" name="password" id="password" class="ml-4 mr-4 w-full rounded-sm" placeholder="password" required="">
+                </div>
+                       
+                
+
+
+
+            </form>
     </div>
 
     <div class="bg-red-300 h-1/5">
