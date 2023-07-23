a6e959de4643c97a996c5a348c9933aadd2c35fc
Golvan98 <gilvinsz@gmail.com>
2023-07-23 15:55:23 +0800

    first half of user profile template laid out, picture missing

--- resources/js/Pages/Index/UserProfile.vue
+++ resources/js/Pages/Index/UserProfile.vue
@@ -1,8 +1,23 @@
 <template>
 
+<div class="h-screen w-full bg-gray-300 flex-nowrap">
 
-hello! template for user profile here
+  <div class="bg-gray-700 w-full h-1/2">
+    <div class="bg-inherit h-1/4">1 </div>
+    <div class="bg-inherit h-1/ ml-4"> Hello Gilvin! <br> <br> You can edit information about yourself here as you please</div>
+    <div class="bg-inherit h-1/4 flex items-end"> <button class="ml-4 bg-blue-300 text-white font-bold text-lg rounded-lg px-2 py-0.5"> Edit Profile</button> </div>
+    <div class="bg-inherit h-1/4">1 </div>
 
+  </div> 
+    
+  <div class="border bg-white w-full h-1/2">
+        
+  </div>
+   
+
+
+
+</div>
 </template>
 
 <script setup>
