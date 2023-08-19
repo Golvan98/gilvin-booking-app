a9fc0bfa4e86f4fb50bac0c1a9b8affa382825ab
Golvan98 <gilvinsz@gmail.com>
2023-08-19 07:19:51 -0400

    added appoint box component and placed it in test modal. Will loop this on existing data next shift

--- resources/js/Pages/Index/Test.vue
+++ resources/js/Pages/Index/Test.vue
@@ -19,7 +19,7 @@
                 <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this profile?
                   <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
                   <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-                  
+                  <AppointmentRequestsModal></AppointmentRequestsModal>
                 
                 </h3>
                 <Link href="/deleteUser" method="delete" as="button"><button data-modal-hide="popup-modal" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
@@ -50,6 +50,8 @@ import { Link } from '@inertiajs/vue3'
   import { usePage } from '@inertiajs/vue3'
   import { computed } from 'vue'
 
+  import AppointmentRequestsModal from '@/Pages/Index/AppointmentRequestsModal.vue'
+  
 
 
 </script>
