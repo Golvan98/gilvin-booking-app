63f12a8d10b8e2625fa58626eb778f8da5b407cb
Golvan98 <gilvinsz@gmail.com>
2023-08-31 07:45:31 -0400

    successfully packed pagination link modal in a component

--- resources/js/Pages/Index/ProfessionalProfile.vue
+++ resources/js/Pages/Index/ProfessionalProfile.vue
@@ -34,7 +34,7 @@
           Add Service 
         </button>
 
-        <TestModal :parentProp="parentData"/>
+      
 
         <!-- Main modal -->
 
@@ -157,54 +157,7 @@
             </span> <br>
             </div>
             <div> <button data-modal-target="requests-modal" data-modal-toggle="requests-modal" class="mr-4 bg-inherit text-start text-xs" preserve-state> Requests </button></div>
-            <div  id="requests-modal" tabindex="-1" class="justify-center bg-inherit fixed top-0 left-0 right-0 z-50 hidden p-4 h-3/4" preserve-state >
-            <div class="relative w-1/4 mx-auto bg-white overflow-y-auto md:inset-0 h-[calc(100%-3rem)]">
-                <div class="relative bg-white  dark:bg-gray-700">
-                    <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="requests-modal">
-                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                        </svg>
-                        <span class="sr-only">Close modal</span>
-                    </button>
-                    
-                    <div class="p-6 text-center max-h-[400px]">
-                      
-                        <h3 class="mb-5 text-lg text-white font-bold bg-blue-500 py-2"> Appointment Requests</h3>
-
-                            <div v-for="(pendingRequests) in pendingRequests.data" id="modalcontent" class="bg-white flex-nowrap justify-center items-center rounded-xl border border-gray-300 mb-2 shadow">
-
-                            <div class="mt-4 flex justify-start ml-2 font-bold"> {{ requests.created_at}}</div>
-
-                            <div class="w-full h-auto flex-nowrap"> 
-                                    <div v-for="(consultees) in consultees" class="flex justify-start ">  <span class="ml-2" v-if="consultees.id == pendingRequests.by_user_id"> {{ consultees.first_name }} {{ consultees.last_name }}</span> </div>
-
-                                    <div class="h-auto bg-white flex-nowrap justify-start items-start mt-2 mb-4 text-left">
-                                        <div class="mx-2"> {{ pendingRequests.request }} </div> 
-                                    </div> 
-                            </div>
-
-                            <div class="flex justify-between font-bold mb-2">
-                              <a :href="`rejectRequest/${pendingRequests.id}`"> <div class="ml-1 text-sm text-red-500">  Reject Request </div> </a>
-                                    <div class="text-sm text-blue-500"> Reschedule Request </div>
-                              <a :href="`approveRequest/${pendingRequests.id}`"> <div class="ml-1 text-sm text-green-500">  Approve Request </div> </a>      
-                               
-                            </div>
-
-                    </div>
-                        
-                            
-                        
-
-                        <div v-if="pendingRequests.data.length" class="w-full flex justify-center mt-8 mb-8" preserve-state>
-                          <Pagination :links="pendingRequests.links" />
-                        </div>
-
-                      
-
-                    </div>
-                </div>
-            </div>
-        </div>
+            <TestModal :pendingRequests="pendingRequests" :consultees="consultees" :requests="requests" preserve-state/>
         
           </div>
 
@@ -265,12 +218,6 @@
   import TestModal from './TestModal.vue';
 
 
-const goToPage = (url) => {
-  router.visit(url, {
-    replace: true, // Preserve the browser history
-    preserveState: true, // Preserve component state
-  });
-};
 
   import { ref } from 'vue'
   const page = usePage()
@@ -282,8 +229,6 @@ const goToPage = (url) => {
   const profEmail = computed ( () => page.props.prof.email)
   const profLastName = computed(() => page.props.prof.last_name)
   const bio = computed(() => page.props.prof.bio)
-
-  const parentData = 'Data from Professional Profile';
 
   
   const profProfession = computed(() => page.props.prof.profession)
--- resources/js/Pages/Index/TestModal.vue
+++ resources/js/Pages/Index/TestModal.vue
@@ -1,14 +1,76 @@
 <template>
 
-    <div class="bg-red-300 p-24">
-        YAWA  <p>{{ parentProp }}</p>
-    
-    </div>
-    </template>
+<div  id="requests-modal" tabindex="-1" class="justify-center bg-inherit fixed top-0 left-0 right-0 z-50 hidden p-4 h-3/4" preserve-state >
+            <div class="relative w-1/4 mx-auto bg-white overflow-y-auto md:inset-0 h-[calc(100%-3rem)]">
+                <div class="relative bg-white  dark:bg-gray-700">
+                    <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="requests-modal">
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                    
+                    <div class="p-6 text-center max-h-[400px]">
+                      
+                        <h3 class="mb-5 text-lg text-white font-bold bg-blue-500 py-2"> Appointment Requests</h3>
+
+                            <div v-for="(pendingRequests) in pendingRequests.data" id="modalcontent" class="bg-white flex-nowrap justify-center items-center rounded-xl border border-gray-300 mb-2 shadow">
+
+                            <div class="mt-4 flex justify-start ml-2 font-bold"> {{ requests.created_at}}</div>
+
+                            <div class="w-full h-auto flex-nowrap"> 
+                                    <div v-for="(consultees) in consultees" class="flex justify-start ">  <span class="ml-2" v-if="consultees.id == pendingRequests.by_user_id"> {{ consultees.first_name }} {{ consultees.last_name }}</span> </div>
+
+                                    <div class="h-auto bg-white flex-nowrap justify-start items-start mt-2 mb-4 text-left">
+                                        <div class="mx-2"> {{ pendingRequests.request }} </div> 
+                                    </div> 
+                            </div>
+
+                            <div class="flex justify-between font-bold mb-2">
+                              <a :href="`rejectRequest/${pendingRequests.id}`"> <div class="ml-1 text-sm text-red-500">  Reject Request </div> </a>
+                                    <div class="text-sm text-blue-500"> Reschedule Request </div>
+                              <a :href="`approveRequest/${pendingRequests.id}`"> <div class="ml-1 text-sm text-green-500">  Approve Request </div> </a>      
+                               
+                            </div>
+
+                    </div>
+                        
+                            
+                        
+
+                        <div v-if="pendingRequests.data.length" class="w-full flex justify-center mt-8 mb-8" preserve-state>
+                          <Pagination :links="pendingRequests.links" />
+                        </div>
+
+                      
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        </template>
     
     <script setup>
    import { defineProps } from 'vue';
+   import { Link } from '@inertiajs/vue3'
+    import { reactive } from 'vue'
+    import { usePage } from '@inertiajs/vue3'
+    import { computed } from 'vue'
+    import { useForm } from '@inertiajs/vue3'
+    import { router } from '@inertiajs/vue3'
+    import ServiceModal from '@/Pages/Index/ServiceModal.vue'
+    import DeleteModal from '@/Pages/Index/DeleteModal.vue'
+    import Bio from '@/Pages/Index/Bio.vue'
+    import Pagination from '@/Pages/Index/Pagination.vue'
+    import TestModal from './TestModal.vue';
+  
+
+
+
    const props = defineProps({
-  parentProp: String
+    pendingRequests: Object,
+    consultees: Object,
+    requests: Object
 });
     </script>
