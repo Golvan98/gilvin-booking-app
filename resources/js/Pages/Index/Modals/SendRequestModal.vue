d551329cb56ad205fe40d2da6f028e0b9603db0b
Golvan98 <gilvinsz@gmail.com>
2023-10-04 09:01:04 -0400

    added own vite code for modal for the front-end part of requesting appointment

--- resources/js/Pages/Index/Modals/SendRequestModal.vue
+++ resources/js/Pages/Index/Modals/SendRequestModal.vue
@@ -1,16 +1,41 @@
 <template>
 
-<div id="request-modal" tabindex="-1" aria-hidden="true"  class="hidden h-screen w-full z-50 fixed inset-0 items-center justify-center overflow-x-hidden overflow-y-auto">
+<div id="request-modal" class="hidden z-50 fixed inset-0 overflow-x-hidden overflow-y-auto rounded-lg">
 
-    <div class="w-full h-1/3 bg-green-300">
+    <div class="flex-nowrap h-auto w-1/4 bg-white rounded-lg px-12 py-4 ">
 
-    </div>
+        <div class="h-1/3 w-full bg-inherit flex-nowrap rounded-t-lg pt-6 bg-white">
+                        
+            <div class="flex items-center justify-center mb-4"> Request </div>
+            <div class="flex items-center justify-center"> 
+                            <input type="text" placeholder="context here" class="w-full rounded-lg">
+            </div>
 
-    <div class="w-full h-1/3 bg-indigo-300">
+        </div>
+        
+        <div class="h-1/3 w-full bg-white pt-6">
 
-    </div>
+            <div class="flex items-center justify-center mb-4"> Appointment Start Time </div>
+            <div class="flex items-center justify-center"> 
+                            <input type="datetime-local" placeholder="context here" class="w-full rounded-lg">
+            </div>  
 
-    <div class="w-full h-1/3 bg-red-300">
+        </div>
+
+        <div class="h-1/3 w-full bg-white py-6">
+
+            <div class="flex items-center justify-center mb-4"> Appointment End Time </div>
+            <div class="flex items-center justify-center"> 
+                            <input type="datetime-local" placeholder="context here" class="w-full rounded-lg">
+            </div>  
+
+        </div>
+
+        <div class="w-full h-12 flex items-center justify-between">
+             <button data-modal-hide="request-modal" class="bg-blue-600 text-white py-1 px-2 rounded-sm"> Cancel </button>
+            <a href="#"><button class="bg-blue-600 text-white py-1 px-2 rounded-sm"> Confirm </button> </a> 
+        </div>
+
 
     </div>
 
