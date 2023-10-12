88fa8af8369f98e06a92b18ef6ff2dbab7736ae1
Golvan98 <gilvinsz@gmail.com>
2023-10-12 07:31:54 -0400

    added feature where it notifies a user if a request was accepted

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -4,16 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Professional;
 
 class NotificationController extends Controller
 {
     public function notification(Request $request)
     {
+       
+
+
         return inertia(
             'Index/Notifications',
             [
-                'notifications' => $request->user()
-                    ->notifications()->paginate(10)
+                'notifications' => $request->user()->notifications()->paginate(10),
+                'professionals' => Professional::all()
             ]
         );
     }
--- resources/js/Pages/Index/Notifications.vue
+++ resources/js/Pages/Index/Notifications.vue
@@ -1,10 +1,39 @@
 <template>
 
-notification page here
+<section v-if="notifications.data.length" class="text-gray-700 dark:text-gray-400">
+    <div v-for="notification in notifications.data" :key="notification.id" class="border-b border-gray-200 dark:border-gray-800 py-4 flex justify-between items-center">
+      <div  v-if="notification.type === 'App\\Notifications\\RequestAccepted'">
+
+        <div v-for="professional in professionals">
+         <span v-if="professional.id==notification.data.by_professional_id">
+            Request to {{ professional.first_name }} was accepted
+        </span>
+
+        </div>
+
+      </div>
+      <div>
+        <button v-if="!notification.read_at" class="btn-outline text-xs font-medium uppercase">
+          Mark as read
+        </button>
+      </div>
+    </div>
+
+    
+  </section>
+
+  <section v-else>
+    No notifications found
+  </section>
 
 
 </template>
 
 <script setup>
-
+import Pagination from '@/Pages/Index/Pagination.vue'
+import { Link } from '@inertiajs/vue3'
+defineProps({
+  notifications: Object,
+  professionals:Object
+})
 </script>
