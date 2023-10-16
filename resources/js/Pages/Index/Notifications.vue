165834cd0f47cf2a682492028cff7f4fcc61e752
Golvan98 <gilvinsz@gmail.com>
2023-10-16 03:16:53 -0400

    modified frontend part for marking notifications as read

--- resources/js/Pages/Index/Notifications.vue
+++ resources/js/Pages/Index/Notifications.vue
@@ -2,21 +2,24 @@
 
 <section v-if="notifications.data.length" class="text-gray-700 dark:text-gray-400">
     <div v-for="notification in notifications.data" :key="notification.id" class="border-b border-gray-200 dark:border-gray-800 py-4 flex justify-between items-center">
-      <div  v-if="notification.type === 'App\\Notifications\\RequestAccepted'">
 
-        <div v-for="professional in professionals">
+      <div v-if="notification.type === 'App\\Notifications\\RequestAccepted'">
+
+        <div v-for="professional in professionals" class="ml-24">
          <span v-if="professional.id==notification.data.by_professional_id">
-            Request to {{ professional.first_name }} was accepted
+            Request to <strong> {{ professional.first_name }} </strong> was accepted
         </span>
 
         </div>
         
       </div>
-      <div>
-        <Link as="button" method="put" :href="`/notification/${notification.id}/seen`" v-if="!notification.read_at" class="btn-outline text-xs font-medium uppercase">
+
+      <div  v-if="!notification.read_at" class="mr-24 border border-black p-2 bg-indigo-700 text-white font-bold">
+        <Link as="button" method="put" :href="`/notification/${notification.id}/seen`" class="btn-outline text-xs font-medium uppercase">
           Mark as read
         </Link>
       </div>
+      
     </div>
 
     
