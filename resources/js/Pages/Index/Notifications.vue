80294bbb823ffbbcf53630b39edf6e93d257d924
Golvan98 <gilvinsz@gmail.com>
2023-10-24 05:56:46 -0400

    added href on notifications to view user/professional profile

--- resources/js/Pages/Index/Notifications.vue
+++ resources/js/Pages/Index/Notifications.vue
@@ -7,7 +7,7 @@
 
         <div v-for="professional in professionals" class="ml-24">
          <span v-if="professional.id==notification.data.by_professional_id">
-            Request to <strong> {{ professional.first_name }} </strong> was <span class="text-green-500 font-bold"> Accepted </span>
+            Request to <a :href="`viewProfessionalProfile/${professional.id}`"> <strong> {{ professional.first_name }}  </strong> </a> was <span class="text-green-500 font-bold"> Accepted </span>
         </span>
 
         </div>
@@ -18,7 +18,7 @@
 
         <div v-for="professional in professionals" class="ml-24">
          <span v-if="professional.id==notification.data.by_professional_id">
-            Request to <strong> {{ professional.first_name }} </strong> was <span class="text-red-500 font-bold"> Rejected </span> 
+            Request to <a :href="`viewProfessionalProfile/${professional.id}`"> <strong> {{ professional.first_name }}  </strong> </a> was <span class="text-red-500 font-bold"> Rejected </span> 
         </span>
 
         </div>
@@ -29,7 +29,7 @@
 
         <div v-for="professional in professionals" class="ml-24">
          <span v-if="professional.id==notification.data.by_professional_id">
-            Appointment to <strong> {{ professional.first_name }} </strong> <span class="text-red-500 font-bold"> Cancelled </span> 
+            Appointment to <a :href="`viewProfessionalProfile/${professional.id}`"> <strong> {{ professional.first_name }}  </strong> </a> <span class="text-red-500 font-bold"> Cancelled </span> 
         </span>
 
         </div>
