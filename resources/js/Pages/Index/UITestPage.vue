ce98d5c497807d564ee8d976adcb5b24666dc3bb
Golvan98 <gilvinsz@gmail.com>
2023-11-13 08:13:57 -0500

    navbar for test UI page done, will try to implement various screen sizes soon

--- resources/js/Pages/Index/UITestPage.vue
+++ resources/js/Pages/Index/UITestPage.vue
@@ -1,6 +1,42 @@
 <template>
 
-Hello testpage here
+<div id="maingrid" class="h-screen w-full grid grid-rows-6 grid-cols-5 bg-green-300">
+
+  <div id="navbar" class="grid grid-rows-6 grid-cols-6 col-span-1 row-span-6 bg-white border border-black">
+
+    <div id="contacts" class="row-span-3 col-span-6 bg-white">
+
+            <div class="flex-nowrap h-full">
+
+                <div class="flex items-end justify-center h-12 font-bold text-2xl mt-4 bg-white"> 
+                    Contact Details 
+                </div> 
+                
+                <div class="flex items-center justify-center w-full"> Email Address </div>
+                <div class="flex items-center justify-center  w-full"> Linkedin </div>
+                <div class="flex items-center justify-center  w-full"> Instagram </div>
+            </div> 
+
+    </div>
+
+    <div id="services/description" class="row-span-3 col-span-6 bg-white">
+
+            <div class="flex-nowrap h-full">
+
+                <div class="flex items-end justify-center h-12 font-bold mt-4 text-2xl bg-white"> 
+                    Verified Services
+                </div> 
+                
+                <div class="flex items-center justify-center w-full"> Civil Engineering</div>
+                <div class="flex items-center justify-center  w-full"> CAD </div>
+                <div class="flex items-center justify-center  w-full"> Consultancy </div>
+            </div> 
+
+    </div>
+
+  </div>
+
+</div>
 
 </template>
 
