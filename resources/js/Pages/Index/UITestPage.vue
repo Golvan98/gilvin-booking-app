6002a49b89228a5bb5ab3ec3e04fa3ad031f8a20
Golvan98 <gilvinsz@gmail.com>
2023-11-20 04:59:24 -0500

    profile content done, will tweak with its sizes soon

--- resources/js/Pages/Index/UITestPage.vue
+++ resources/js/Pages/Index/UITestPage.vue
@@ -38,24 +38,52 @@
 
   <div id="profileContent" class="col-span-4 row-span-6 bg-red-300 grid grid-cols-2 grid-rows-6">
 <!-- u put md:size around here, flex centers wont matter I think -->
-      <div id="profilePic" class="bg-green-300 col-span-2 row-span-4 border border-red-300 items-center justify-center flex">
+      <div id="profilePic" class="bg-white col-span-2 row-span-4 border border-red-300 items-center justify-center flex border-b-green-700">
 
-        <div class="w-1/3 h-3/5 bg-green-400">
-         hehe
+        <div class="w-1/3 h-3/5"
+          style="background-image: url('storage/aiGeneratedPic.png'); 
+            background-repeat: no-repeat; 
+            background-size: cover
+            border-color: black;">
+         
         </div>
       </div>
 
-      <div id="shortDescription" class="col-span-1 row-span-1 bg-indigo-700">
+      <div id="shortDescription" class="col-span-1 row-span-1 bg-white flex-nowrap">
+
+        <div class="flex w-full h-12 items-center justify-start font-bold text-4xl ml-2">
+        John Doe
+        </div>
+
+        <div class="flex w-full h-6 items-start justify-start ml-2">
+        Palao, Iligan City
+        </div>
+        <div class="flex w-full h-6 items-start justify-start ml-2 text-sm">
+        I make shitty websites as a startup junior developer
+        </div>
+       
 
       </div>
 
       <div id="links" class="col-span-1 row-start-6 bg-green-300">
+
+        <div class="flex w-full h-12 items-center justify-start font-bold text-4xl ml-2">
+        Links
+        </div>
+
+        <div class="flex text-blue-700 hover:text-blue-500 w-full h-6 items-start justify-start ml-2 text-sm">
+          <a href="https://www.linkedin.com/in/gilvin-zalsos-213692141/" target="blank"> Linkedin </a>
+        </div>
+        <div class="flex text-blue-700 hover:text-blue-500 w-full h-6 items-start justify-start ml-2 text-sm">
+          <a href="https://github.com/Golvan98" target="blank"> Github </a>
+        </div>
 
       </div>
 
   </div>
 
 </div>
+
 
 </template>
 
@@ -66,3 +94,4 @@
 
 
 </script>
+
