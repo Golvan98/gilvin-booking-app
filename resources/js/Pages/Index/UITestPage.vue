b2c69f367d2e92b4f77e753fcd5aa336b8c5289d
Golvan98 <gilvinsz@gmail.com>
2023-11-20 06:00:16 -0500

    created mx margins for navbar, will set its two sections next time to h-1/2s

--- resources/js/Pages/Index/UITestPage.vue
+++ resources/js/Pages/Index/UITestPage.vue
@@ -1,20 +1,25 @@
 <template>
 
-<div id="maingrid" class="h-screen w-full grid grid-rows-6 grid-cols-5 bg-green-300">
+<div id="maingrid" class="h-screen w-full grid grid-rows-6 grid-cols-5">
 
-  <div id="navbar" class="grid grid-rows-6 grid-cols-6 col-span-1 row-span-6 bg-white border border-black">
+  <div id="navbar" class="grid grid-rows-6 grid-cols-6 col-span-1 row-span-6 bg-white border border-gray-300">
 
     <div id="contacts" class="row-span-3 col-span-6 bg-white">
 
-            <div class="flex-nowrap h-full">
+            <div class="flex-nowrap h-full bg-red-300">
 
-                <div class="flex items-end justify-center h-12 font-bold text-2xl mt-4 bg-white"> 
-                    Contact Details 
+                <div class="flex-nowrap items-end justify-center font-bold text-2xl mt-4 h-full bg-red-500"> 
+                   <div> Contact Details </div> 
+               
+                  <div class="flex "> Email Address  </div>
+                  <div class="flex text-blue-700"> gilvinsz@gmail.com  </div>
+                  <div class="flex"> Linkedin </div>
+                  <div class="flex"> Instagram </div>
+
+
                 </div> 
                 
-                <div class="flex items-center justify-center w-full"> Email Address </div>
-                <div class="flex items-center justify-center  w-full"> Linkedin </div>
-                <div class="flex items-center justify-center  w-full"> Instagram </div>
+             
             </div> 
 
     </div>
@@ -27,23 +32,23 @@
                     Verified Services
                 </div> 
                 
-                <div class="flex items-center justify-center w-full"> Civil Engineering</div>
-                <div class="flex items-center justify-center  w-full"> CAD </div>
-                <div class="flex items-center justify-center  w-full"> Consultancy </div>
+                <div class="flex items-center w-full mx-25-percent"> Civil Engineering</div>
+                <div class="flex items-center w-full mx-25-percent"> CAD </div>
+                <div class="flex items-center w-full mx-25-percent"> Consultancy </div>
             </div> 
 
     </div>
 
   </div>
 
-  <div id="profileContent" class="col-span-4 row-span-6 bg-red-300 grid grid-cols-2 grid-rows-6">
+  <div id="profileContent" class="col-span-4 row-span-6 grid grid-cols-2 grid-rows-6">
 <!-- u put md:size around here, flex centers wont matter I think -->
       <div id="profilePic" class="bg-white col-span-2 row-span-4 border border-red-300 items-center justify-center flex border-b-green-700">
 
-        <div class="w-1/3 h-3/5"
+        <div class="w-1/3 h-3/5 border border-red-300"
           style="background-image: url('storage/aiGeneratedPic.png'); 
             background-repeat: no-repeat; 
-            background-size: cover
+            background-size: cover;
             border-color: black;">
          
         </div>
@@ -65,7 +70,7 @@
 
       </div>
 
-      <div id="links" class="col-span-1 row-start-6 bg-green-300">
+      <div id="links" class="col-span-1 row-start-6">
 
         <div class="flex w-full h-12 items-center justify-start font-bold text-4xl ml-2">
         Links
@@ -94,4 +99,18 @@
 
 
 </script>
+<style>
+
+  .my-25-percent {
+    margin-top: 25%;
+    margin-bottom: 25%;
+  }
+
+  .mx-25-percent {
+    margin-left: 25%;
+    margin-right: 25%;
+  }
+  
+
+  </style>
 
