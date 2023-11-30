200496fd252259f11ce9f8231c16752b19eaa516
Golvan98 <gilvinsz@gmail.com>
2023-11-30 07:31:33 -0500

    polished profile content, will transfer this as the new profile page

--- resources/js/Pages/Index/UITestPage.vue
+++ resources/js/Pages/Index/UITestPage.vue
@@ -1,8 +1,8 @@
 <template>
 
-<div id="wrapper" class="w-full h-screen with-background border flex items-center">
+<div id="wrapper" class="w-full h-screen with-background flex items-center">
 
-  <div id="maingrid" class="grid grid-rows-6 grid-cols-5 border border-purple-400 w-5/6 h-5/6 mx-auto ">
+  <div id="maingrid" class="grid grid-rows-6 grid-cols-5 w-5/6 h-5/6 mx-auto ">
     
 
     <div id="navbar" class="grid grid-rows-6 h-full grid-cols-6 col-span-1 row-span-6 bg-white w-full ">
@@ -11,29 +11,27 @@
 
               <div class="flex-nowrap h-full">
 
-                  <div class="grid grid-cols-2 sm:text-xs md:text-sm lg:text-2xl grid-rows-4 items-center justify-center mx-auto w-full bg-white h-full font-bold mt-4"> 
+                  <div class="grid grid-cols-2 sm:text-xs md:text-sm lg:text-xl grid-rows-4 items-center justify-center mx-auto w-full bg-white h-full font-bold mt-4"> 
 
-                    <div class="row-start-1 sm:col-span-2 md:col-span-2 lg:col-span-2 sm:text-xs md:text-sm lg:text-2xl lg:text-black mx-auto font-bold"> Contact Details </div> 
+                    <div class="row-start-1 sm:text-red-700 md:text-yellow-300 lg:text-green-700 sm:col-span-2 md:col-span-2 lg:col-span-2 sm:text-xs md:text-sm lg:text-2xl mx-auto font-bold"> Contact Details </div> 
                     
-                    <div class="grid grid-cols-1 grid-rows-2 row-start-2 col-span-2 mx-2 border-b border-gray-300"> 
+                    <div class="grid grid-cols-1 grid-rows-2 row-start-2 col-span-2 mx-3 border-b border-gray-300"> 
                      <div class="row-span-1 mx-auto"> Email Address  </div> 
                      <div class="row-span-1 text-blue-400 mx-auto"> gilvinsz@gmail.com  </div> 
                     </div>
                     
-                    <div class="grid grid-cols-1 grid-rows-2 row-start-3 col-span-2 mx-2 border-b border-gray-300"> 
+                    <div class="grid grid-cols-1 grid-rows-2 row-start-3 col-span-2 mx-3 border-b border-gray-300"> 
                      <div class="row-span-1 mx-auto"> Linkedin  </div> 
                      <div class="row-span-1 text-blue-400 text-xs mx-auto"> linkedin.com/in/gilvin-zalsos-213692141/  </div> 
                     </div>
 
-
-                    <div class="grid grid-cols-1 grid-rows-2 row-start-4 col-span-2 mx-2 border-b border-gray-300 mb-3"> 
+                    <div class="grid grid-cols-1 grid-rows-2 row-start-4 col-span-2 mx-3 border-b border-gray-300 mb-3"> 
                      <div class="row-span-1 mx-auto"> Skype  </div> 
                      <div class="row-span-1 text-blue-400 text-xs mx-auto"> live:gilvinsz </div> 
                     </div>
 
                   </div> 
-                  
-              
+                               
               </div> 
 
       </div>
@@ -42,31 +40,28 @@
 
               <div class="flex-nowrap h-full ">
 
-                  <div class="grid grid-cols-2 sm:text-xs md:text-sm lg:text-2xl grid-rows-6 items-center justify-center mx-auto w-full bg-white h-full font-bold"> 
+                  <div class="grid grid-cols-2 sm:text-xs md:text-sm lg:text-xl grid-rows-6 items-center justify-center mx-auto w-full bg-white h-full font-bold"> 
 
                     <div class="row-span-1 sm:col-span-2 md:col-span-2 lg:col-span-2 sm:text-xs md:text-sm lg:text-2xl lg:text-black mx-auto font-bold"> Services </div> 
                     
-                    <div class="grid grid-cols-1 grid-rows-1 row-span-1 col-span-2 mx-2 border-b border-gray-300"> 
+                    <div class="grid grid-cols-1 grid-rows-1 row-span-1 col-span-2 mx-3 border-b border-gray-300"> 
                      <div class="row-span-1 mx-auto"> Generic Service 1  </div> 
                     </div>
 
-                    <div class="grid grid-cols-1 grid-rows-1 row-span-1 col-span-2 mx-2 border-b border-gray-300"> 
+                    <div class="grid grid-cols-1 grid-rows-1 row-span-1 col-span-2 mx-3 border-b border-gray-300"> 
                      <div class="row-span-1 mx-auto"> Generic Service 2  </div> 
                     </div>
 
-                     <div class="grid grid-cols-1 grid-rows-1 row-span-1 col-span-2 mx-2 border-b border-gray-300"> 
+                     <div class="grid grid-cols-1 grid-rows-1 row-span-1 col-span-2 mx-3 border-b border-gray-300"> 
                      <div class="row-span-1 mx-auto"> Generic Service 3 </div> 
                     </div>
-                     <div class="grid grid-cols-1 grid-rows-1 row-span-1 col-span-2 mx-2 border-b border-gray-300"> 
+                     <div class="grid grid-cols-1 grid-rows-1 row-span-1 col-span-2 mx-3 border-b border-gray-300"> 
                      <div class="row-span-1 mx-auto"> Generic Service 4  </div> 
                     </div>
-                     <div class="grid grid-cols-1 grid-rows-1 row-span-1 col-span-2 mx-2 border-b border-gray-300"> 
+                     <div class="grid grid-cols-1 grid-rows-1 row-span-1 col-span-2 mx-3 border-b border-gray-300"> 
                      <div class="row-span-1 mx-auto"> Generic Service 5  </div> 
                     </div>
                     
-                    
-                   
-
                   </div> 
 
               </div> 
@@ -75,11 +70,11 @@
 
     </div>
 
-    <div id="profileContent" class="col-span-4 row-span-6 grid grid-cols-2 grid-rows-6 h-full">
+    <div id="profileContent" class="col-span-4 row-span-6 grid grid-cols-2 grid-rows-6 h-full border-l border-gray-300">
   <!-- u put md:size around here, flex centers wont matter I think -->
-        <div id="profilePic" class="bg-white col-span-2 row-span-4 border border-red-300 items-center justify-center flex border-b-green-700">
+        <div id="profilePic" class="bg-white col-span-2 row-span-4 items-center justify-center flex ">
 
-          <div class="w-2/5 h-4/5 border border-red-300"
+          <div class="sm:w-3/5 sm:h-4/5 md:w-3/5 md:h-5/6 h-full lg:w-2/5 lg:h-5/6"
             style="background-image: url('storage/aiGeneratedPic.png'); 
               background-repeat: no-repeat; 
               background-size: cover;
@@ -88,17 +83,17 @@
           </div>
         </div>
 
-        <div id="shortDescription" class="col-span-2 row-span-1 bg-white flex-nowrap">
+        <div id="shortDescription" class="col-span-2 row-span-1 bg-white flex-nowrap border-t border-gray-300">
 
-          <div class="bg-white flex w-full h-12 items-center justify-start font-bold text-4xl">
-          John Doe
+          <div class="bg-white flex w-full h-12 items-center justify-start font-bold sm:text-xl md:text-2xl lg:text-3xl">
+            <span class="ml-2"> John Doe </span>
           </div>
 
           <div class="bg-white flex w-full h-6 items-start justify-start">
-          Palao, Iligan City
+            <span class="ml-2"> Palao, Iligan City</span>
           </div>
           <div class="bg-white flex w-full h-6 items-start justify-start text-sm">
-          I make shitty websites as a startup junior developer
+            <span class="ml-2">  I make shitty websites as a startup junior developer</span>
           </div>
         
 
@@ -106,15 +101,15 @@
 
         <div id="links" class="bg-white col-span-2 row-start-6">
 
-          <div class="flex w-full h-12 items-center justify-start font-bold text-4xl">
-          Links
+          <div class="sm:text-xl md:text-2xl lg:text-3xl flex w-full h-12 items-center justify-start font-bold text-4xl">
+            <span class="ml-2">Links</span> 
           </div>
 
           <div class="bg-white flex text-blue-700 hover:text-blue-500 w-full h-6 items-start justify-start text-sm">
-            <a href="https://www.linkedin.com/in/gilvin-zalsos-213692141/" target="blank"> Linkedin </a>
+            <a href="https://www.linkedin.com/in/gilvin-zalsos-213692141/" target="blank" class="ml-2"> Linkedin </a>
           </div>
           <div class="bg-white flex text-blue-700 hover:text-blue-500 w-full h-6 items-start justify-start text-sm">
-            <a href="https://github.com/Golvan98" target="blank"> Github </a>
+            <a href="https://github.com/Golvan98" target="blank" class="ml-2"> Github </a>
           </div>
 
         </div>
