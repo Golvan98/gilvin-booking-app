c0c9191e69910c89bb7b453358ffcffb2390036e
Golvan98 <gilvinsz@gmail.com>
2023-12-14 09:37:59 -0500

    added profile pic part for profileBlock with margins in professional's test page

--- resources/js/Pages/Index/UITestPage.vue
+++ resources/js/Pages/Index/UITestPage.vue
@@ -2,7 +2,7 @@
 
   <div id="mainDiv" class="flex items-center justify-center h-screen w-full bg-gray-100">
   
-    <div id="2ndDivContainer" class="bg-white w-2/3 h-4/6 border border-red-700">
+    <div id="contentContainer" class="bg-white w-2/3 h-4/6 border border-red-700">
 
       <div id="searchBarContainer" class="flex items-center justify-center h-1/5 w-full bg-indigo-300 ">
 
@@ -10,31 +10,24 @@
 
       </div>
 
-      <div id="profilesMainContainer" class="w-3/4 h-3/5 grid grid-cols-3 grid-rows-4 m-auto justify-center items-center">
+      <div id="profilesMainContainer" class="w-3/4 h-3/5 grid grid-cols-3 grid-rows-4 m-auto justify-center items-center space-x-2">
 
-        <div class="profileBlock col-span-1 row-span-2 bg-red-300 m-auto border w-full h-full flex justify-center items-center">
-            <div> atay </div>
+        <div class="profileBlock col-span-1 row-span-2 rounded-xl bg-green-100 m-auto border w-full h-full flex justify-between">
+            <div :style="{ 
+                           'background-image': 'url(\'http://127.0.0.1:8000/storage/default.png\')',
+                        'background-repeat': 'no-repeat', 
+                        'background-size': 'cover' 
+                    }"
+                    class="w-1/3 h-1/2 rounded-full 
+                            inline-flex items-center justify-center 
+                            bg-gray-400 text-gray-700 text-xl font-bold shadow-sm ml-2 mt-4 border border-green-300">  
+            </div>
+
+            <div class="w-full bg-red-300"> </div>
         </div>
+
+        
      
-        <div class="profileBlock col-span-1 row-span-2 bg-red-300 m-auto border w-full h-full">
-            <div> atay </div>
-        </div>
-        
-        <div class="profileBlock col-span-1 row-span-2 bg-red-300 m-auto border w-full h-full">
-            <div> atay </div>
-        </div>
-        
-        <div class="profileBlock col-span-1 row-span-2 bg-red-300 m-auto border w-full h-full">
-            <div> atay </div>
-        </div>
-        
-        <div class="profileBlock col-span-1 row-span-2 bg-red-300 m-auto border w-full h-full">
-            <div> atay </div>
-        </div>
-        
-        <div class="profileBlock col-span-1 row-span-2 bg-red-300 m-auto border w-full h-full">
-            <div> atay </div>
-        </div>
         
 
         
