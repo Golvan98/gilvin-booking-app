4ef23af5a62aaf6901a0d69eda81e2816c085ed0
Golvan98 <gilvinsz@gmail.com>
2023-12-21 05:10:50 -0500

    created 3rd iteration of search navbar for professional list

--- resources/js/Pages/Index/Filters2.vue
+++ resources/js/Pages/Index/Filters2.vue
@@ -4,21 +4,34 @@
 <form id="searchBarContent" class="w-3/4 flex items-center h-3/4 justify-center">
 
   <div id="searchBar" class="w-1/3 h-full flex justify-center items-center">
-      <div class="flex items-center justify-center w-full h-3/4"> 
-        <div class="w-1/2 h-3/4 items-center lg:font-medium md:text-sm sm:text-xs"> 
+      <div class="flex items-center justify-center w-1/2 h-1/2"> 
+        <div class="w-full h-full items-center lg:font-medium md:text-sm sm:text-xs"> 
          <input class="w-full h-full" type="text" placeholder="search user">
          </div>
       </div>
   </div>
 
-  <div id="sortBy" class="w-1/3 h-full bg-red-300 flex justify-center items-center">
-      hello
+  <div id="sortBy" class="w-1/3 h-full flex justify-end items-center">
+      <div class=" w-1/2 h-1/2 flex items-center justify-end"> 
+        <select>
+        <option value="Legal"> Legal </option>
+        <option value="Doctor"> Doctor </option>
+        <option value="Engineer"> Engineer </option>
+        <option value="Accountant"> Accountant </option>
+        <option value="IT"> IT </option>
+        <option value="Tutor"> Tutor </option>
+        <option value="Misc">  Misc </option>
+        </select>
+      </div>
   </div>
 
-  <div id="SubmitClearButtons" class="w-1/3 h-full bg-red-300 flex items-center">
-      submitMe
+  <div id="SubmitClearButtons" class="w-1/3 h-full flex items-center justify-center">
+    <div class="w-1/2 h-1/2 flex items-center justify-center space-x-1"> 
+    <button class="h-5/6 bg-white rounded-sm sm:text-xs md:text-sm lg:text-sm sm:w-full md:w-full lg:w-1/2 sm:p-1 md:p-0.5 lg:p-0.5"> Submit </button>
+    <button  class="w-1/2 h-5/6 bg-white rounded-sm sm:text-xs md:text-sm lg:text-sm sm:w-full md:w-full lg:w-1/2 sm:p-1 md:p-0.5 lg:p-0.5"> Clear</button>
+    </div> 
   </div>
-    
+
 
 </form>
     
